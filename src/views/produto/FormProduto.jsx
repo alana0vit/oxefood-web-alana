92d0705bf72f6807bf381ba0d891c360e8fd8ad9
alana0vit <alana.vit.ms@gmail.com>
2026-02-22 00:15:06 -0300

    feat: criado a tela de FormProduto

--- src/views/produto/FormProduto.jsx
+++ src/views/produto/FormProduto.jsx
@@ -1,8 +1,121 @@
 import InputMask from 'comigo-tech-react-input-mask';
-import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
+import { Button, Container, Divider, Form, FormField, Icon, TextArea } from 'semantic-ui-react';
 
-export default function FormProduto () {
+export default function FormProduto() {
+    return (
 
-    
+        <div>
 
+            <div style={{ marginTop: '3%' }}>
+
+                <Container textAlign='justified' >
+
+                    <h2> <span style={{ color: 'darkgray' }}> Produto &nbsp;<Icon name='angle double right' size="small" /> </span> Cadastro </h2>
+
+                    <Divider />
+
+                    <div style={{ marginTop: '4%' }}>
+
+                        <Form>
+
+                            <Form.Group>
+
+                                <Form.Input
+                                    required
+                                    fluid
+                                    label='Título'
+                                    width={12}
+                                    maxLength="50"
+                                >
+                                    <InputMask
+                                        placeholder="Informe o título do produto"
+                                    />
+                                </Form.Input>
+
+                                <Form.Input
+                                    required
+                                    fluid
+                                    width={6}
+                                    label='Código do Produto'
+                                >
+                                    <InputMask
+                                        placeholder="Informe o código do produto"
+                                    />
+                                </Form.Input>
+
+                            </Form.Group>
+
+                            <FormField
+                                control={TextArea}
+                                label='Descrição'
+                                placeholder='Informe a descrição do produto'
+                            />
+
+                            <Form.Group>
+
+                                <Form.Input
+                                    fluid
+                                    required
+                                    label='Valor Unitário'
+                                    width={6}>
+                                </Form.Input>
+
+                                <Form.Input
+                                    fluid
+                                    label='Tempo de Entrega Mínimo em Minutos'
+                                    width={6}>
+                                    <InputMask
+                                        placeholder="30"
+                                    />
+                                </Form.Input>
+
+                                <Form.Input
+                                    fluid
+                                    label='Tempo de Entrega Máximo em Minutos'
+                                    width={6}
+                                >
+                                    <InputMask
+                                        placeholder="40"
+                                    />
+                                </Form.Input>
+
+                            </Form.Group>
+
+                        </Form>
+
+                        <div style={{ marginTop: '4%' }}>
+
+                            <Button
+                                type="button"
+                                inverted
+                                circular
+                                icon
+                                labelPosition='left'
+                                color='orange'
+                            >
+                                <Icon name='reply' />
+                                Voltar
+                            </Button>
+
+                            <Button
+                                inverted
+                                circular
+                                icon
+                                labelPosition='left'
+                                color='blue'
+                                floated='right'
+                            >
+                                <Icon name='save' />
+                                Salvar
+                            </Button>
+
+                        </div>
+
+                    </div>
+
+                </Container>
+            </div>
+        </div>
+
+    );
 }
